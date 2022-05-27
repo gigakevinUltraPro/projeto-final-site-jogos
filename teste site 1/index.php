20ecb81be2c37d402ef2c4d4afc7fac17b9aeeb9
gigakevinUltraPro <102774702+gigakevinUltraPro@users.noreply.github.com>
2022-05-27 11:41:35 -0300

    Add files via upload

---
+++ teste site 1/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="small.css" />
+    <link rel="stylesheet" href="medium.css" />
+    <link rel="stylesheet" href="large.css" />
+    <title>CHAD GAMES</title>
+
+    <title>NOTICIAS</title>
+</head>
+
+<header>
+  <h1>Noticia sobre jogos</h1>
+</header>
+
+<main>
+  <h2>
+    Novas noticias
+    [<a href="nova_noticia.php">Novo</a>]
+  </h2>
+
+  <table>
+    <tr>
+      <th>
+        <p>numero da noticia</p>
+      </th>
+      <th>
+        <p>Descrição</p>
+      </th>
+      <th>
+        <p>Alterar</p>
+      </th>
+      <th>
+        <p>Excluir</p>
+      </th>
+    </tr>
+
+<?php 
